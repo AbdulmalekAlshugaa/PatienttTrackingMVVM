20d14c92a364a351fa38d352f1fcf99b29e38bd0
Abdulmalik Alhuugaa <abdulmalik@troopers.com.my>
2021-01-14 01:35:13 +0800

    cam

---
+++ app/src/main/java/com/example/osamah/Fragments/camare.java
@@ -0,0 +1,76 @@
+package com.example.osamah.Fragments;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.provider.MediaStore;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.osamah.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link camare#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class camare extends Fragment {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+    static final int REQUEST_VIDEO_CAPTURE = 1;
+    View view;
+
+    // TODO: Rename and change types of parameters
+
+
+    public camare() {
+        // Required empty public constructor
+    }
+    private void dispatchTakeVideoIntent() {
+        Intent takeVideoIntent = new Intent(MediaStore.ACTION_VIDEO_CAPTURE);
+        if (takeVideoIntent.resolveActivity(getActivity().getPackageManager()) != null) {
+            startActivityForResult(takeVideoIntent, REQUEST_VIDEO_CAPTURE);
+        }
+    }
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment camare.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static camare newInstance(String param1, String param2) {
+        camare fragment = new camare();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        view =  inflater.inflate(R.layout.fragment_camare, container, false);
+      //  dispatchTakeVideoIntent();
+
+        return view;
+    }
+}
